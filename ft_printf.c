b9bfb7a82144627c9f58a1d194a4801d5e50f484
amaeldb <70023468+amaeldb@users.noreply.github.com>
2021-12-16 16:31:24 +0100

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -6,21 +6,43 @@
 /*   By: ade-beta <ade-beta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 15:03:42 by ade-beta          #+#    #+#             */
-/*   Updated: 2021/12/15 16:27:13 by ade-beta         ###   ########.fr       */
+/*   Updated: 2021/12/16 16:30:55 by ade-beta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int	conv(const char c, va_list par)
+{
+	int	count;
+
+	count = 
+
 int	ft_printf(const char *str, ...)
 {
 	va_list	par;
 	int		i;
+	int		count;
 
 	i = -1;
 	va_start(par, str);
 	while (str[++i])
 	{
-		
+		if (str[i] == '%' && str[i + 1])
+		{
+			if (ft_strchr("cspdiuxX%", str[i + 1]))
+				count += conv(str[++i], par);
+			else
+			{
+				write(1, str[i], 1);
+				write(1, str[++i], 1);
+				count += 2;
+			}
+			continue ;
+		}
+		write(1, str[i], 1);
+		count++;
 	}
+	va_end(par);
+	return (count);
 }
--- main.c
+++ main.c
@@ -5,5 +5,5 @@
 int main(void)
 {
 	char *str = "Wazzzaaaaaaaa?!";
-	printf("%c\n%s\n%p\n%d\n%i\n%u\n%x\n%X\n%%\n", 69, NULL, -1, 69, 69, 69, 42, 42);
+	printf("%c\n%s\n%p\n%d\n%i\n%u\n%x\n%X\n%%\n%\n", 68, NULL, -1, 6, 9, -1, 42, 42);
 }
