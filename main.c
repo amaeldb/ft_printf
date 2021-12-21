d178d78fb5cfa2215f02e3ea913d5bfcda24e38a
amaeldb <70023468+amaeldb@users.noreply.github.com>
2021-12-21 14:40:19 +0100

    Delete main.c

--- main.c
+++
@@ -1,9 +0,0 @@
-#include "ft_printf.h"
-
-#include <stdio.h>
-
-int main(void)
-{
-	char *str = "Wazzzaaaaaaaa?!";
-	printf("%c\n%s\n%p\n%d\n%i\n%u\n%x\n%X\n%%\n%\n", 68, NULL, -1, 6, 9, -1, 42, 42);
-}
